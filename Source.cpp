acb8087024a23b05dbe365ef3d66b72f863f6104
SirPerpetuum <84527074+SirPerpetuum@users.noreply.github.com>
2023-01-09 05:52:34 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+template <typename T>
+void write_vector(const vector<vector<T>> &V)
+{
+    cout << "The numbers in the vector are: " << endl;
+    for (int i = 0; i < V.size(); i++)
+    {
+        for (int j = 0; j < V[i].size(); j++)
+            cout << V[i][j] << " ";
+        cout << endl;
+    }
+}
+
+void main()
+{
+    int input;
+    vector<int> V; //вводим данные в вектор
+    vector<int>::iterator vec1;
+    vector<int> vec2;
+    vector<vector<int>> vec;
+
+    cout << "Enter your numbers: " << endl;
+    while (cin >> input)
+        V.push_back(input);
+
+    sort(V.begin(), V.end());  //сортировка данных
+
+    vec.push_back(V);  // заполнили 0 индекс вектора
+
+    vec1 = unique(vec[0].begin(), vec[0].end()); 
+    vec[0].erase(vec1, vec[0].end()); //вектор уникальных чисел
+
+    for (vector<int>::iterator vec_it = V.begin(), next; vec_it != V.end(); vec_it = next)
+    {
+        next = upper_bound(vec_it, V.end(), *vec_it);
+        vec2.push_back(distance(vec_it, next));  // вектор количества 
+    }
+
+    vec.push_back(vec2);
+
+    write_vector(vec);
+}
